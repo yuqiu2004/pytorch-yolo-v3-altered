465ff473dc5374482d94cbce46d82ef98e2bf972
yuqiu <2506852507@qq.com>
2024-12-19 15:26:26 +0800

    feat : modify on id

--- pytorchyolo/detect_l.py
+++ pytorchyolo/detect_l.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python3
-
 from __future__ import division
-
 import glob
 import os
 import argparse
@@ -10,88 +8,38 @@ import random
 import numpy as np
 from datetime import datetime
 import shutil
-
 from PIL import Image
-
 import torch
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
-
 from pytorchyolo.models import load_model
 from pytorchyolo.utils.utils import load_classes, rescale_boxes, non_max_suppression, print_environment_info
 from pytorchyolo.utils.datasets import ImageFolder
 from pytorchyolo.utils.transforms import Resize, DEFAULT_TRANSFORMS
-
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.ticker import NullLocator
 
-
-def detect_directory(model_path, weights_path, img_path, classes, output_path,
+def detect_single_image(model_path, weights_path, img_path, classes, output_path,
                      batch_size=8, img_size=416, n_cpu=8, conf_thres=0.5, nms_thres=0.5):
-    """Detects objects on all images in specified directory and saves output images with drawn detections.
-
-    :param model_path: Path to model definition file (.cfg)
-    :type model_path: str
-    :param weights_path: Path to weights or checkpoint file (.weights or .pth)
-    :type weights_path: str
-    :param img_path: Path to directory with images to inference
-    :type img_path: str
-    :param classes: List of class names
-    :type classes: [str]
-    :param output_path: Path to output directory
-    :type output_path: str
-    :param batch_size: Size of each image batch, defaults to 8
-    :type batch_size: int, optional
-    :param img_size: Size of each image dimension for yolo, defaults to 416
-    :type img_size: int, optional
-    :param n_cpu: Number of cpu threads to use during batch generation, defaults to 8
-    :type n_cpu: int, optional
-    :param conf_thres: Object confidence threshold, defaults to 0.5
-    :type conf_thres: float, optional
-    :param nms_thres: IOU threshold for non-maximum suppression, defaults to 0.5
-    :type nms_thres: float, optional
-    """
-    # 加载图像数据
-    dataloader = _create_data_loader(img_path, batch_size, img_size, n_cpu)
-    # 加载模型
-    model = load_model(model_path, weights_path)
-    # 调用detect执行批量检测
-    img_detections, imgs = detect(
-        model,
-        dataloader,
-        output_path,
-        conf_thres,
-        nms_thres,
-        img_size)
-    # 绘制检测结果并保存
-    _draw_and_save_output_images(
-        img_detections, imgs, img_size, output_path, classes)
-    if os.path.exists(output_path) and not os.listdir(output_path):  # 检查目录是否存在且为空
-        shutil.rmtree(output_path)  # 删除目录
-        print(f"---- no result is output and directory '{output_path}' has been removed ----")
-    # 有结果输出
-    else :
-        print(f"---- Detections were saved to: '{output_path}' ----")
-        print(f"---- my name: 王强 Wangqiang ----")
-
-def detect_simgle_image(model_path, weights_path, img_path, classes, output_path,
-                     batch_size=8, img_size=416, n_cpu=8, conf_thres=0.5, nms_thres=0.5):
-    # 处理图片路径
-    img_path = sorted(glob.glob(os.path.join(img_path, '*.*')))
+    # 处理图片路径 获取路径下的第一张图片
+    img_path = sorted(glob.glob("%s/*.*" % img_path))[0]
+    img = np.array(
+            Image.open(img_path).convert('RGB'),
+            dtype=np.uint8)
     # 加载模型
     model = load_model(model_path, weights_path)
     # 调用detect_image 单个检测
-    img_detections = detect_image(
+    img_detection = detect_image(
         model,
-        output_path,
+        img,
         conf_thres,
         nms_thres,
         img_size)
     # 绘制检测结果并保存
-    _draw_and_save_output_images(
-        img_detections, img_path, img_size, output_path, classes)
+    _draw_and_save_output_image(
+        img_detection, img_path, img_size, output_path, classes)
     print(f"---- Detections were saved to: '{output_path}' ----")
     print(f"---- my name: 王强 Wangqiang ----")
 
@@ -112,7 +60,6 @@ def detect_image(model, image, img_size=416, conf_thres=0.5, nms_thres=0.5):
     :rtype: nd.array
     """
     model.eval()  # Set model to evaluation mode
-
     # Configure input
     input_img = transforms.Compose([
         DEFAULT_TRANSFORMS,
@@ -128,120 +75,6 @@ def detect_image(model, image, img_size=416, conf_thres=0.5, nms_thres=0.5):
         detections = non_max_suppression(detections, conf_thres, nms_thres)
         detections = rescale_boxes(detections[0], img_size, image.shape[:2])
     return detections.numpy()
-
-
-# 对输入的 DataLoader 中的图像进行推理。
-# 执行非极大值抑制（NMS）筛选检测框。
-# 输出检测结果和对应的图像路径。
-def detect(model, dataloader, output_path, conf_thres, nms_thres, img_size, multiple_min=3):
-    """Inferences images with model.
-
-    :param model: Model for inference
-    :type model: models.Darknet
-    :param dataloader: Dataloader provides the batches of images to inference
-    :type dataloader: DataLoader
-    :param output_path: Path to output directory
-    :type output_path: str
-    :param conf_thres: Object confidence threshold, defaults to 0.5
-    :type conf_thres: float, optional
-    :param nms_thres: IOU threshold for non-maximum suppression, defaults to 0.5
-    :type nms_thres: float, optional
-    :param img_size: Size of each image dimension for yolo, defaults to 416
-    :type img_size: int, optional
-    :param multiple_min: minimum allowed size multiple and if bigger than it then cut-handle, defaults to 3
-    :type multiple_min: float, optional
-    :return: List of detections. The coordinates are given for the padded image that is provided by the dataloader.
-        Use `utils.rescale_boxes` to transform them into the desired input image coordinate system before its transformed by the dataloader),
-        List of input image paths
-    :rtype: [Tensor], [str]
-    """
-    # Create output directory, if missing
-    os.makedirs(output_path, exist_ok=True)
-
-    model.eval()  # Set model to evaluation mode
-
-    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
-
-    img_detections = []  # Stores detections for each image index
-    imgs = []  # Stores image paths
-
-    # 定义和之前等价的操作
-    transform = transforms.Compose([DEFAULT_TRANSFORMS, Resize(img_size)])
-    boxes = np.zeros((1, 5))
-    for (img_paths, input_imgs) in tqdm.tqdm(dataloader, desc="Detecting"):
-        # Configure input
-        height, width = input_imgs.shape[-2:]  # Height, Width
-        area = height * width  # 计算宽度和高度的乘积
-        """
-        if area > img_size ** 2 * multiple_min : # 如果大于阈值 则需要剪切检测
-            print("=====>img size is over the threshold<=====")
-            # 分别预测 然后组合detections...
-            # 定义默认的预处理操作
-            transform = transforms.Compose([DEFAULT_TRANSFORMS, 
-                                            transforms.Resize((img_size, img_size))])
-            # 对图像进行预处理和缩放
-            input_imgs = transform(input_imgs, [])
-            input_imgs = Variable(input_imgs.type(Tensor))
-            # Get detections
-            with torch.no_grad():
-                detections = model(input_imgs)
-                detections = non_max_suppression(detections, conf_thres, nms_thres)
-            # Store image and detections
-            img_detections.extend(detections)
-            imgs.extend(img_paths)
-        else :
-            print("=====>img size is below the threshold<=====")
-            # 定义默认的预处理操作
-            transform = transforms.Compose([DEFAULT_TRANSFORMS, 
-                                            Resize(img_size)])
-            # 对图像进行预处理和缩放
-            input_imgs = transform(input_imgs)
-            input_imgs = Variable(input_imgs.type(Tensor))
-            # Get detections
-            with torch.no_grad():
-                detections = model(input_imgs)
-                detections = non_max_suppression(detections, conf_thres, nms_thres)
-            # Store image and detections
-            img_detections.extend(detections)
-            imgs.extend(img_paths)
-        """
-        print(input_imgs)
-        return img_detections, imgs
-        
-        # 对图像进行预处理和缩放
-        input_imgs, _ = transform((input_imgs, boxes))
-        input_imgs = Variable(input_imgs.type(Tensor))
-        # Get detections
-        with torch.no_grad():
-            detections = model(input_imgs)
-            detections = non_max_suppression(detections, conf_thres, nms_thres)
-        # Store image and detections
-        img_detections.extend(detections)
-        imgs.extend(img_paths)
-    return img_detections, imgs
-
-
-def _draw_and_save_output_images(img_detections, imgs, img_size, output_path, classes):
-    """Draws detections in output images and stores them.
-
-    :param img_detections: List of detections
-    :type img_detections: [Tensor]
-    :param imgs: List of paths to image files
-    :type imgs: [str]
-    :param img_size: Size of each image dimension for yolo
-    :type img_size: int
-    :param output_path: Path of output directory
-    :type output_path: str
-    :param classes: List of class names
-    :type classes: [str]
-    """
-
-    # Iterate through images and save plot of detections
-    for (image_path, detections) in zip(imgs, img_detections):
-        print(f"Image {image_path}:")
-        _draw_and_save_output_image(
-            image_path, detections, img_size, output_path, classes)
-
 
 def _draw_and_save_output_image(image_path, detections, img_size, output_path, classes):
     """Draws detections in output image and stores this.
@@ -300,35 +133,6 @@ def _draw_and_save_output_image(image_path, detections, img_size, output_path, c
     plt.savefig(output_path, bbox_inches="tight", pad_inches=0.0)
     plt.close()
 
-# 加载图像目录，生成 PyTorch 的 DataLoader 对象。
-# 应用 Resize 和 DEFAULT_TRANSFORMS 对图像进行预处理。
-def _create_data_loader(img_path, batch_size, img_size, n_cpu):
-    """Creates a DataLoader for inferencing.
-
-    :param img_path: Path to file containing all paths to validation images.
-    :type img_path: str
-    :param batch_size: Size of each image batch
-    :type batch_size: int
-    :param img_size: Size of each image dimension for yolo
-    :type img_size: int
-    :param n_cpu: Number of cpu threads to use during batch generation
-    :type n_cpu: int
-    :return: Returns DataLoader
-    :rtype: DataLoader
-    """
-    # 对图片预处理 搞成指定大小的
-    dataset = ImageFolder(
-        img_path)
-        # transform=transforms.Compose([DEFAULT_TRANSFORMS, Resize(img_size)]) 
-        # 不做统一的缩放操作 要根据图片的大小来决定
-    dataloader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=n_cpu,
-        pin_memory=True)
-    return dataloader
-
 # 使用 argparse 解析命令行参数。
 # 加载类别名。
 # 调用 detect_directory 执行检测任务。
@@ -348,22 +152,9 @@ def run():
     parser.add_argument("--nms_thres", type=float, default=0.4, help="IOU threshold for non-maximum suppression")
     args = parser.parse_args()
     print(f"Command line arguments: {args}")
-
     # Extract class names from file
     classes = load_classes(args.classes)  # List of class names
-
-    # detect_directory(
-    #     args.model,
-    #     args.weights,
-    #     args.images,
-    #     classes,
-    #     args.output,
-    #     batch_size=args.batch_size,
-    #     img_size=args.img_size,
-    #     n_cpu=args.n_cpu,
-    #     conf_thres=args.conf_thres,
-    #     nms_thres=args.nms_thres)
-    detect_simgle_image(args.model,
+    detect_single_image(args.model,
                  args.weights,
                 args.images,
                 classes,
@@ -373,7 +164,6 @@ def run():
                 n_cpu=args.n_cpu,
                 conf_thres=args.conf_thres,
                 nms_thres=args.nms_thres)
-
 
 if __name__ == '__main__':
     run()
